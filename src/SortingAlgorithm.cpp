2aa307198d36e5adcc8c7fec132ca665b3b394e9
dhkien1510 <170169279+dhkien1510@users.noreply.github.com>
2025-03-16 16:39:20 +0700

    Update command line 1

--- src/SortingAlgorithm.cpp
+++ src/SortingAlgorithm.cpp
@@ -175,6 +175,7 @@ void CountingSort :: sortImp(int arr[], int n){
 
   int* cnt = new int[maxElement + 1]{0};
   int* res = new int[n]{0};
+  
   for(int i = 0; ++count_compare && i < n; i++)
     cnt[arr[i]]++;
 
@@ -199,17 +200,23 @@ void RadixSort :: sortImp(int arr[], int n){
     if(arr[i] > maxElement)
       maxElement = arr[i];
   }
+
   int* res = new int[n];
+
   for(int exp = 1; ++count_compare && maxElement / exp > 0; exp *= 10){
       int cnt[10]{0};
+
       for(int i = 0; ++count_compare && i < n; i++)
-          cnt[(arr[i]/exp) % 10]++;
+          cnt[(arr[i] / exp) % 10]++;
+
       for(int i = 1; ++count_compare && i < 10; i++)
           cnt[i] += cnt[i - 1];
+
       for(int i = n - 1; ++count_compare && i >= 0; i--){
           res[cnt[(arr[i] / exp) % 10] - 1] = arr[i];
-          cnt[(arr[i]/exp) % 10]--;
+          cnt[(arr[i] / exp) % 10]--;
       }
+
       for(int i = 0; ++count_compare && i < n; i++)
           arr[i] = res[i];     
       
